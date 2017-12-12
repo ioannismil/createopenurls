1a1c2e227dff19e87bccef9b5b5685b0eba4c159
IoannisM <miltiadouy@gmail.com>
2017-12-13 01:40:06 +0200

    New
    Added lists, option not too add numbers

--- function.js
+++ function.js
@@ -4,8 +4,16 @@ var nx = document.getElementById("num").value;
 var i;
 var ex = document.getElementById("ext").value;
 var txt="";
+var ckb = document.getElementById("chk");
+var lst = document.getElementById("lst").value;
+var slst = lst.split(" ");
     for(i=0;i<=nx;i++){
-        txt+='<a href="'+x+i+ex+'">'+x+i+ex+'</a>'+"<br>";   
+        if(ckb.checked == true){
+        txt+='<a href="'+x+i+slst[i]+ex+'">'+x+i+slst[i]+ex+'</a>'+"<br>";
+        }
+        else {
+            txt+='<a href="'+x+slst[i]+ex+'">'+x+slst[i]+ex+'</a>'+"<br>";
+        }
     }
 document.getElementById("hld").innerHTML=txt;
 }
@@ -14,10 +22,17 @@ var x = document.getElementById("txt").value;
 var nx = document.getElementById("num").value;
 var i;
 var ex = document.getElementById("ext").value;
+var ckb = document.getElementById("chk");
+var lst = document.getElementById("lst").value;
+var slst = lst.split(" ");
 var LinksArray = new Array();
     for(i=0;i<=nx;i++){
-        LinksArray[i]=x+i+ex;
-        
+        if(ckb.checked == true){
+            LinksArray[i]=x+i+slst[i]+ex;
+        }
+        else{
+            LinksArray[i]=x+slst[i]+ex;
+        }
     }
       for (i=0; i<LinksArray.length; i++) {
         window.open(LinksArray[i]);
