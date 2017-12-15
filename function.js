2e1453b71253f9f7259a0ff796efe4a232e69487
IoannisM <miltiadouy@gmail.com>
2017-12-15 18:21:09 +0200

    Added CSS
    Added CSS

--- function.js
+++ function.js
@@ -1,31 +1,52 @@
 function calculate(){
 var x = document.getElementById("txt").value;
 var nx = document.getElementById("num").value;
-var i;
-var ex = document.getElementById("ext").value;
-var txt="";
 var ckb = document.getElementById("chk");
 var lst = document.getElementById("lst").value;
+var ex = document.getElementById("ext").value;
+var i;
+var txt="";
 var slst = lst.split(" ");
+    
+if(slst.length==1){
+        for(i=0;i<=nx;i++){
+            slst[i]="";
+        }
+    }
     for(i=0;i<=nx;i++){
         if(ckb.checked == true){
-        txt+='<a href="'+x+i+slst[i]+ex+'">'+x+i+slst[i]+ex+'</a>'+"<br>";
+        txt+='<a href="'+x+i+slst[i]+ex+'">'+x+i+slst[i]+ex+'</a>'+"<br>"; 
         }
         else {
             txt+='<a href="'+x+slst[i]+ex+'">'+x+slst[i]+ex+'</a>'+"<br>";
         }
     }
+        var x = document.getElementById("con");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 document.getElementById("hld").innerHTML=txt;
 }
+
 function openall(){
+    
 var x = document.getElementById("txt").value;
 var nx = document.getElementById("num").value;
-var i;
 var ex = document.getElementById("ext").value;
 var ckb = document.getElementById("chk");
 var lst = document.getElementById("lst").value;
+var i;
 var slst = lst.split(" ");
 var LinksArray = new Array();
+    
+if(slst.length==1){
+        for(i=0;i<=nx;i++){
+            slst[i]="";
+        }
+    }
+
     for(i=0;i<=nx;i++){
         if(ckb.checked == true){
             LinksArray[i]=x+i+slst[i]+ex;
@@ -34,6 +55,7 @@ var LinksArray = new Array();
             LinksArray[i]=x+slst[i]+ex;
         }
     }
+    
       for (i=0; i<LinksArray.length; i++) {
         window.open(LinksArray[i]);
   }
